14fef787c80ab8595c8fdebb26b57a8c4f70560c
SUPPORT\suppo <hundo1217@gmail.com>
2026-04-06 15:08:04 +0900

    Revert "Feat: 근태관리시스템"
    This reverts commit 41ed80e1e5b932ff22b8a0c8e14c758adcafbd21.

--- backend/src/main/java/com/kintai/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.kintai.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String employeeCode;
-    private String password;
-}
--- backend/src/main/java/com/kintai/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kintai.repository;
-
-import com.kintai.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}
